bb50bc76dbed3e4431470e0a18f16db11c97ec7f
Cedric <cda609@AFPA-PARIS-DI>
2021-02-09 09:41:55 +0100

    eliminer eleve déjà choisis

--- choix_eleve.js
+++ choix_eleve.js
@@ -1,12 +1,14 @@
 var classe = ["Camille","Cédric","Dimitri","Ingrid","Josse","Lucas","Moussa", "Nadia","Nawfal","Rim","Simon", "Sonia", "Vlad"];
-
+var classe_pas_encore_selectionne = [].concat(classe);
 var bouton_choix = document.querySelector("#choisir_qui");
 var affichage_nom = document.querySelector("#choix_fait");
 bouton_choix.onclick = choisir_quelquun;
 
 function choisir_quelquun(){
-	var index_choisi = Math.floor(Math.random() * classe.length);
-	affichage_nom.innerHTML = classe[index_choisi];
+	var index_choisi = Math.floor(Math.random() * classe_pas_encore_selectionne.length);
+	affichage_nom.innerHTML = classe_pas_encore_selectionne[index_choisi];
+	var splice = classe_pas_encore_selectionne.splice(index_choisi,1);
+	classe_pas_encore_selectionne.length == 0 ? classe_pas_encore_selectionne = [].concat(classe) :"";
 }
 
 var bouton_choix_stress = document.querySelector("#choisir_qui_stress");
@@ -20,10 +22,13 @@ function choisir_quelquun_stress(){
 		setTimeout(function(){
 		chargement_factice.innerHTML += '<div class="spinner-grow text-danger" style="width: 3rem; height: 3rem; animation-duration: 1s; animation-delay: 300ms;" role="status"></div>';
 		}, 1000*index);
-	}
+	} 
 	setTimeout(function(){
-		var index_choisi = Math.floor(Math.random() * classe.length);
-		affichage_nom_stress.innerHTML = classe[index_choisi];
+		var index_choisi = Math.floor(Math.random() * classe_pas_encore_selectionne.length);
+		affichage_nom_stress.innerHTML = classe_pas_encore_selectionne[index_choisi];
 		chargement_factice.innerHTML = "";
+		classe_pas_encore_selectionne.splice(index_choisi,1);
+		classe_pas_encore_selectionne.length == 0 ? classe_pas_encore_selectionne = [].concat(classe):"";
+		console.log(classe_pas_encore_selectionne);
 	}, 6000);
 }
